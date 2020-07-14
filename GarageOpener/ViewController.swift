99e1f0e48131334e261ebf1034de3cf317653689
muealexi <alexis@voliro.ch>
2020-07-14 17:14:05 +0200

    Initial Commit

---
+++ GarageOpener/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GarageOpener
+//
+//  Created by Alexis Müller on 14.07.20.
+//  Copyright © 2020 Alexis Müller. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
